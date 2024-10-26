4a038f403f5598588d72a660cf561182d9f835d8
TAYLAK1 <mumin12169@gmail.com>
2024-10-26 17:43:11 +0600

    added new py file

---
+++ samples.py
@@ -0,0 +1,14 @@
+cars = ['mers', 'audi', 'volvo', 'bmw', 'honda']
+mers = ['m210', 'm211', 'm212']
+audi = ['a100', 'a80', 'a6']
+print(cars)
+word = input('name car: ').lower()
+
+if word == mers:
+    print(mers)
+
+
+
+
+
+
